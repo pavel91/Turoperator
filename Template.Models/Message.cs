0ae10b5f308d14b365c5d634cc6491476946fea6
pippo <pippo@ANDROMEDA>
2016-04-24 13:40:10 +0300

    Message Model created

--- Template.Models/Message.cs
+++ Template.Models/Message.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Template.Models
+﻿namespace Template.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Template.Models.Enums;
+
     public class Message
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        ApplicationUser Sender { get; set; }
+
+        [Required]
+        ApplicationUser Recipient { get; set; }
+
+        [Required]
+        DateTime SentTime { get; set; }
+
+        [Required]
+        MessageStatus Status { get; set; }
     }
 }
